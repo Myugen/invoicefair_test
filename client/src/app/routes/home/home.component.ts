1443040119049502ff873aaace2d55ff2b5e8fac
Miguel Cabrera <miguelcabsan@gmail.com>
2021-04-09 19:00:11 +0100

    feat(parent): Add request to balance when user is selected

--- client/src/app/routes/home/home.component.ts
+++ client/src/app/routes/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BalanceService } from 'src/app/core/services/balance.service';
+import { Balance } from 'src/app/models/balance';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html'
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomeComponent implements OnInit {
   public name: string;
-  constructor() { }
+  public mail: string;
+  public totalBalance: number;
+
+  constructor(private balanceService: BalanceService) {}
 
   ngOnInit(): void {
     this.name = 'Parent';
+  }
+
+  onSelectUsername(username: string): void {
+    this.balanceService.findBalance(username).subscribe((balance) => {
+      this.mail = balance?.account.reference;
+      this.totalBalance = balance?.totals?.reduce((accumulator, current) => accumulator + (current.balance * current.toEurRate), 0);
+    });
   }
 }
