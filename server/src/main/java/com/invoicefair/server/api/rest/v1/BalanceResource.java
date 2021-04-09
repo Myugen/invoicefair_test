efe511ea1f4131875de9894c3e7af9c53317a87d
Miguel Cabrera <miguelcabsan@gmail.com>
2021-04-09 17:46:16 +0100

    feat(rest): Update balance resource api
    > Adding functionality to balance resource.

--- server/src/main/java/com/invoicefair/server/api/rest/v1/BalanceResource.java
+++ server/src/main/java/com/invoicefair/server/api/rest/v1/BalanceResource.java
@@ -1,16 +1,34 @@
 package com.invoicefair.server.api.rest.v1;
 
 import com.invoicefair.server.api.rest.v1.vo.responses.BalanceResponse;
+import com.invoicefair.server.domain.Balance;
+import com.invoicefair.server.services.BalanceService;
+import javassist.NotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(AbstractApiV1Resource.path + "/balances")
-public class BalanceResource extends AbstractApiV1Resource{
+@RestController
+@RequestMapping(BalanceResource.PATH)
+@AllArgsConstructor
+public class BalanceResource extends AbstractApiV1Resource {
 
-    @GetMapping("/:mail")
-    public ResponseEntity<BalanceResponse> findOne(@RequestParam("mail") String mail) {
-        return ResponseEntity.ok(new BalanceResponse());
+    public static final String PATH = AbstractApiV1Resource.path + "/balances";
+
+    public final BalanceService balanceService;
+
+    @GetMapping("/{username}")
+    public ResponseEntity<BalanceResponse> findOne(@PathVariable("username") String username) {
+        BalanceResponse response;
+        try {
+            Balance balance = balanceService.findOne(username);
+            response = BalanceResponse.fromDomain(balance);
+        } catch (NotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(response);
     }
 }
--- server/src/main/java/com/invoicefair/server/api/rest/v1/vo/responses/BalanceResponse.java
+++ server/src/main/java/com/invoicefair/server/api/rest/v1/vo/responses/BalanceResponse.java
@@ -1,5 +1,9 @@
 package com.invoicefair.server.api.rest.v1.vo.responses;
 
+import com.invoicefair.server.domain.Account;
+import com.invoicefair.server.domain.Balance;
+import com.invoicefair.server.domain.Total;
+import com.invoicefair.server.enums.Currency;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,46 +11,47 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class BalanceResponse {
-    private List<Total> totals;
-    private Account account;
+    private AccountResponse account;
+    private List<TotalResponse> totals;
+
+    public static BalanceResponse fromDomain(Balance balance) {
+        AccountResponse account = AccountResponse.fromDomain(balance.getAccount());
+        List<TotalResponse> totals = balance.getTotals().stream().map(TotalResponse::fromDomain).collect(Collectors.toList());
+
+        return new BalanceResponse(account, totals);
+    }
 
     @Getter
     @Setter
-    private static class Total {
-        private enum Currency {
-            EUR(BigDecimal.ONE), USD(BigDecimal.valueOf(0.89));
-
-            private final BigDecimal toEurRate;
-
-            Currency(BigDecimal rate) {
-                this.toEurRate = rate;
-            }
-
-            public BigDecimal getToEurRate() {
-                return toEurRate;
-            }
-        }
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    private static class TotalResponse {
 
         private Currency currency;
         private BigDecimal balance;
         private BigDecimal toEurRate;
 
-        private Total(Currency currency, BigDecimal balance) {
-            this.currency = currency;
-            this.balance = balance;
-            this.toEurRate = currency.getToEurRate();
+        private static TotalResponse fromDomain(Total total) {
+            BigDecimal toEurRate = total.to(Currency.EUR);
+            return new TotalResponse(total.getCurrency(), total.getBalance(), toEurRate);
         }
     }
 
     @Getter
     @Setter
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    private static class Account {
+    private static class AccountResponse {
         private String reference;
         private String displayName;
+
+        private static AccountResponse fromDomain(Account account) {
+            String reference = account.getReference();
+            String displayName = String.format("%s %s", account.getName(), account.getLastName());
+            return new AccountResponse(reference, displayName);
+        }
     }
 }
